b3131dfa85348c8a543400efcf9f1a69afe62056
fatemayahia17 <fatemamohamed650@gmail.com>
2023-03-16 14:59:40 -0700

    done

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,12 @@
-#include <stdio.h>
 #include <unistd.h>
+#include <stdio.h>
 /**
  * main - Entry point
  * Description: C program that print in std error
- * Return:  1 (Success)
+ * Return: 1 (Success)
 */
-
 int main(void)
 {
-	char in[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(1, in, 59);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
