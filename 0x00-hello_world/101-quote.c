a01d9e09d22365fc80d79695bf7b38a6317a41c0
fatemayahia17 <fatemamohamed650@gmail.com>
2023-03-16 14:42:12 -0700

    done

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,7 @@
 
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	char in = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(1, in, 59);
 	return (1);
 }
