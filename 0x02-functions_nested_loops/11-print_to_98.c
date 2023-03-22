faab7d146f8a048ff6c994215886931fa8450c73
fatemayahia17 <fatemamohamed650@gmail.com>
2023-03-22 06:53:10 -0700

    11. 98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_to_98 - function that prints numbers to 98
+ * @n: checks input of function
+ * Return: Always 0 (Successed)
+ */
+
+void print_to_98(int n)
+{
+	int i;
+
+	if (n <= 98)
+	{
+		for (i = n; i <= 97; ++i)
+			printf("%d, ", i);
+	}
+	else
+	{
+		for (i = n; i >= 97; --i)
+			printf("%d, ", i);
+	}
+	printf("%d", 98);
+}
