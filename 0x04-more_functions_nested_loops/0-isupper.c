c0847eba892a641ae89171b47ae953a849f0818c
fatemayahia17 <fatemamohamed650@gmail.com>
2023-03-24 14:28:11 -0700

    0. isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *_isupper - check if input is uper or lower.
+ *@c: input to check
+ * Return: Always 0.
+ */
+int _isupper(int c)
+{
+	int i;
+
+	if (i >= 65 && i <= 90)
+		return (1);
+	else
+		return (0);
+}
