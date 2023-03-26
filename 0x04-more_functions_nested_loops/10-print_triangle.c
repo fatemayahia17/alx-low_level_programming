d1c8c5b1edbf463f829cf2144b78277f0430047c
fatemayahia17 <fatemamohamed650@gmail.com>
2023-03-25 17:46:19 -0700

    done

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -12,16 +12,19 @@ void print_triangle(int size)
 	if (size <= 0)
 		_putchar('\n');
 
-	for (i = 1; i <= size; ++i)
+	else
 	{
-		for (k = 1; k <= size - i; ++k)
+		for (i = 1; i <= size; ++i)
 		{
-			putchar(' ');
+			for (k = 1; k <= size - i; ++k)
+			{
+				_putchar(' ');
+			}
+			for (j = 1; j <= i; ++j)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
 		}
-		for (j = 1; j <= i; ++j)
-		{
-			_putchar('#');
-		}
-		_putchar('\n');
 	}
 }
