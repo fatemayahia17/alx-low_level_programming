b0afc68e9b313bbaaae4d6235751b05924a99723
fatemayahia17 <fatemamohamed650@gmail.com>
2023-03-27 18:51:06 -0700

    pointers, arrays, and strings done!

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * rev_string - the function name
+ * strlen  - it gets the length of a string from string.h library
+ * len - variable
+ * @s: String parameter
+ */
+
+void rev_string(char *s)
+{
+
+	int len = strlen(s);
+	int i = 0;
+
+	while (i < len / 2)
+	{
+		char temp = s[i];
+		
+		s[i] = s[len - 1 - i];
+		s[len - 1 - i] = temp;
+		++i;
+	}
+}
