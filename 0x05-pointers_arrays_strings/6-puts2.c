8370db25b60f6c4fbc60e1f85921d2ab6aada6f5
fatemayahia17 <fatemamohamed650@gmail.com>
2023-03-27 19:15:03 -0700

    pointers, arrays, and strings done!

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -5,15 +5,13 @@
  * _putchar - a header function that inputs character
  * @str: string parameter
  */
-
 void puts2(char *str)
 {
-	int i = 0;
+	int i;
 
-	while (str[i] != '\0')
+	for (i = 0; str[i] != '\0'; i += 2)
 	{
 		_putchar(str[i]);
-		i += 2;
 	}
 	_putchar('\n');
 }
