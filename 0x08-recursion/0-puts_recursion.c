17c25c67d7bb0493e7cf285f764e841d191baa03
fatemayahia17 <fatemamohamed650@gmail.com>
2023-04-05 16:41:41 -0700

    Recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
  /**
- * _puts_recursion - Prints a string followed by a new line
+ *_puts_recursion - function Prints a string followed by a new line
  * @s: string
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
