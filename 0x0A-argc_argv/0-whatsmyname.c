7f502410697fbd42fa2c446d8da3e8d462c1199d
fatemayahia17 <fatemamohamed650@gmail.com>
2023-04-10 18:00:04 -0700

    argc, argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints program name with new line
+ * @argc: int first input
+ * @argv: list second input
+ * Return: 0
+ */
+
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints number of arguments passed
+ * @argc: number of command line arguments
+ * @argv:pointer to an array of command line arguments
+ * Return: 0
+ */
+
+int main(int argc, char  *argv[])
+{
+	(void)argc;
+	printf("%d\n", agc - 1);
+	return (0);
+}
