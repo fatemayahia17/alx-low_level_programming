c18eb5bad7cc5179f880ffc3fbaa235e42a76a38
fatemayahia17 <fatemamohamed650@gmail.com>
2023-04-10 19:58:40 -0700

    f

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -3,19 +3,16 @@
  *main -  prints number of arguments passed into program
  * @argc: int
  * @argv: list
- * Reaurn: 0
+ * Return: 0
  */
 int main(int argc, char const *argv[])
 {
 	int i = 0;
 
-	if (args > 0)
-	{
-		while (i < argc)
+		while (argc--)
 		{
 			printf("%s\n", argv[i]);
 			++i;
 		}
-	}
 	return (0);
 }
